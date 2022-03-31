21638343214563cbce40bf784464a713c299c7e8
hyesoo-ahn <ahngptn719@gmail.com>
2022-03-31 14:14:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    connectWS();
+  }, []);
+  let socket;
+
+  // 웹소켓 연결
+  const connectWS = () => {
+    if (socket != undefined) {
+      socket.close();
+    }
+    console.log("connecting ...", "wss://pubwss.bithumb.com/pub/ws");
+
+    socket = new WebSocket("wss://pubwss.bithumb.com/pub/ws");
+
+    socket.onopen = function (e) {
+      console.log("onopen...");
+    };
+
+    // socket.onclose = function (e) {
+    //   console.log("onclose...");
+    //   // $("#session_id").val("");
+    //   socket = undefined;
+    // };
+    socket.onmessage = function (e) {
+      console.log("onmessage...", e.data);
+    };
+  };
+
+  // 웹소켓 연결 해제
+  function closeWS() {
+    if (socket != undefined) {
+      console.log("closing requested");
+      socket.close();
+      socket = undefined;
+    }
+  }
+
+  // 웹소켓 요청
+  function filterRequest(filter) {
+    if (socket == undefined) {
+      alert("no connect exists");
+      return;
+    }
+
+    socket.send(filter);
+  }
+
+  const test = async () => {
+    // connectWS();
+    filterRequest(
+      JSON.stringify({
+        type: "ticker",
+        symbols: ["BTC_KRW", "ETH_KRW"],
+        tickTypes: ["30M", "1H", "12H", "24H", "MID"],
+      })
+    );
+
+    // closeWS();
+  };
+
+  // // run test()
+  // test();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ height: 200, display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <h1 style={{ cursor: "pointer" }} onClick={test}>
+        웹소켓 요청 Click
+      </h1>
     </div>
   );
 }
