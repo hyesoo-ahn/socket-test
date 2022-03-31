aff2d2686b727e6067a7e4008d8c2046f970aa69
hyesoo-ahn <ahngptn719@gmail.com>
2022-03-31 22:53:02 +0900

    3/31 render state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({
+    BTC_KRW: {},
+    ETH_KRW: {},
+  });
   useEffect(() => {
     connectWS();
   }, []);
@@ -21,13 +25,17 @@ function App() {
       console.log("onopen...");
     };
 
-    // socket.onclose = function (e) {
-    //   console.log("onclose...");
-    //   // $("#session_id").val("");
-    //   socket = undefined;
-    // };
+    socket.onclose = function (e) {
+      console.log("onclose...");
+      socket = undefined;
+    };
     socket.onmessage = function (e) {
-      console.log("onmessage...", e.data);
+      const parsedData = JSON.parse(e.data);
+      let tempOb = { ...data };
+      tempOb[parsedData.content.symbol] = parsedData;
+
+      console.log(tempOb);
+      setData(tempOb);
     };
   };
 
@@ -63,17 +71,39 @@ function App() {
     // closeWS();
   };
 
-  // // run test()
-  // test();
-
   return (
     <div
       className="App"
-      style={{ height: 200, display: "flex", justifyContent: "center", alignItems: "center" }}
+      style={{
+        // height: 200,
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
       <h1 style={{ cursor: "pointer" }} onClick={test}>
         웹소켓 요청 Click
       </h1>
+
+      <div
+        style={{
+          // display: "flex",
+          // justifyContent: "space-between",
+          // alignItems: "center",
+          width: "50%",
+        }}
+      >
+        <div style={{ width: "50%", border: "1px solid red" }}>
+          <h2>BTC_KRW</h2>
+          {JSON.stringify(data["BTC_KRW"])}
+        </div>
+        <div style={{ width: "50%", border: "1px solid red" }}>
+          <h2>ETH_KRW</h2>
+          {JSON.stringify(data["ETH_KRW"])}
+        </div>
+      </div>
     </div>
   );
 }
